18437c742d6818df58de5b31a4791f58a6681c38
GodfredAsa <goldenheroe1@yahoo.com>
2022-03-13 19:14:41 +0000

    updating the reqparse to belong to the class to keep DRY

--- app.py
+++ app.py
@@ -15,21 +15,21 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank")
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price',
-                            type=float,
-                            required=True,
-                            help="This field cannot be left blank")
-
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'item': "An item with name '{}' already exists. ".format(name)}, 400
-        data = parser.parse_args()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -40,13 +40,7 @@ class Item(Resource):
         return {'message': "'{}' deleted".format(name)}
 
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price',
-                            type=float,
-                            required=True,
-                            help="This field cannot be left blank")
-        data = parser.parse_args()
-
+        data = Item.parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
